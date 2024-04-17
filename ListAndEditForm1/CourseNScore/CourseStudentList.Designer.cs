81cfd09436b4abffcea88ca54acd621fbcb654b3
Your Name <huudanhnguyen1709@gmail.com>
2024-04-17 22:32:28 +0700

    done saveDB selectedCourse

---
+++ ListAndEditForm1/CourseNScore/CourseStudentList.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace ListAndEditForm1.CourseNScore
+{
+    partial class CourseStudentList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.semesterlabel = new System.Windows.Forms.Label();
+            this.txtnamecourse = new System.Windows.Forms.TextBox();
+            this.stdBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.qLSVDBDataSet3 = new ListAndEditForm1.QLSVDBDataSet3();
+            this.btnprint = new System.Windows.Forms.Button();
+            this.stdTableAdapter = new ListAndEditForm1.QLSVDBDataSet3TableAdapters.stdTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.qLSVDBDataSet5 = new ListAndEditForm1.QLSVDBDataSet5();
+            this.qLSVDBDataSet5BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.stdBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.stdTableAdapter1 = new ListAndEditForm1.QLSVDBDataSet5TableAdapters.stdTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bdateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.selectedCourseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.stdBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVDBDataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVDBDataSet5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVDBDataSet5BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stdBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name :";
+            // 
+            // semesterlabel
+            // 
+            this.semesterlabel.AutoSize = true;
+            this.semesterlabel.Font = new System.Drawing.Font("Agency FB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.semesterlabel.Location = new System.Drawing.Point(632, 33);
+            this.semesterlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.semesterlabel.Name = "semesterlabel";
+            this.semesterlabel.Size = new System.Drawing.Size(93, 26);
+            this.semesterlabel.TabIndex = 1;
+            this.semesterlabel.Text = "Semester : ";
+            // 
+            // txtnamecourse
+            // 
+            this.txtnamecourse.Font = new System.Drawing.Font("Agency FB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnamecourse.Location = new System.Drawing.Point(309, 30);
+            this.txtnamecourse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtnamecourse.Multiline = true;
+            this.txtnamecourse.Name = "txtnamecourse";
+            this.txtnamecourse.Size = new System.Drawing.Size(246, 41);
+            this.txtnamecourse.TabIndex = 2;
+            // 
+            // stdBindingSource
+            // 
+            this.stdBindingSource.DataMember = "std";
+            this.stdBindingSource.DataSource = this.qLSVDBDataSet3;
+            // 
+            // qLSVDBDataSet3
+            // 
+            this.qLSVDBDataSet3.DataSetName = "QLSVDBDataSet3";
+            this.qLSVDBDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btnprint
+            // 
+            this.btnprint.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnprint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnprint.Font = new System.Drawing.Font("Agency FB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnprint.Location = new System.Drawing.Point(452, 441);
+            this.btnprint.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnprint.Name = "btnprint";
+            this.btnprint.Size = new System.Drawing.Size(212, 45);
+            this.btnprint.TabIndex = 4;
+            this.btnprint.Text = "Print";
+            this.btnprint.UseVisualStyleBackColor = false;
+            this.btnprint.Click += new System.EventHandler(this.btnprint_Click);
+            // 
+            // stdTableAdapter
+            // 
+            this.stdTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.fnameDataGridViewTextBoxColumn,
+            this.lnameDataGridViewTextBoxColumn,
+            this.bdateDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.genderDataGridViewTextBoxColumn,
+            this.phoneDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.selectedCourseDataGridViewTextBoxColumn,
+            this.pictureDataGridViewImageColumn});
+            this.dataGridView1.DataSource = this.stdBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(46, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1052, 318);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // qLSVDBDataSet5
+            // 
+            this.qLSVDBDataSet5.DataSetName = "QLSVDBDataSet5";
+            this.qLSVDBDataSet5.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // qLSVDBDataSet5BindingSource
+            // 
+            this.qLSVDBDataSet5BindingSource.DataSource = this.qLSVDBDataSet5;
+            this.qLSVDBDataSet5BindingSource.Position = 0;
+            // 
+            // stdBindingSource1
+            // 
+            this.stdBindingSource1.DataMember = "std";
+            this.stdBindingSource1.DataSource = this.qLSVDBDataSet5BindingSource;
+            // 
+            // stdTableAdapter1
+            // 
+            this.stdTableAdapter1.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // fnameDataGridViewTextBoxColumn
+            // 
+            this.fnameDataGridViewTextBoxColumn.DataPropertyName = "fname";
+            this.fnameDataGridViewTextBoxColumn.HeaderText = "fname";
+            this.fnameDataGridViewTextBoxColumn.Name = "fnameDataGridViewTextBoxColumn";
+            // 
+            // lnameDataGridViewTextBoxColumn
+            // 
+            this.lnameDataGridViewTextBoxColumn.DataPropertyName = "lname";
+            this.lnameDataGridViewTextBoxColumn.HeaderText = "lname";
+            this.lnameDataGridViewTextBoxColumn.Name = "lnameDataGridViewTextBoxColumn";
+            // 
+            // bdateDataGridViewTextBoxColumn
+            // 
+            this.bdateDataGridViewTextBoxColumn.DataPropertyName = "bdate";
+            this.bdateDataGridViewTextBoxColumn.HeaderText = "bdate";
+            this.bdateDataGridViewTextBoxColumn.Name = "bdateDataGridViewTextBoxColumn";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            // 
+            // genderDataGridViewTextBoxColumn
+            // 
+            this.genderDataGridViewTextBoxColumn.DataPropertyName = "gender";
+            this.genderDataGridViewTextBoxColumn.HeaderText = "gender";
+            this.genderDataGridViewTextBoxColumn.Name = "genderDataGridViewTextBoxColumn";
+            // 
+            // phoneDataGridViewTextBoxColumn
+            // 
+            this.phoneDataGridViewTextBoxColumn.DataPropertyName = "phone";
+            this.phoneDataGridViewTextBoxColumn.HeaderText = "phone";
+            this.phoneDataGridViewTextBoxColumn.Name = "phoneDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // selectedCourseDataGridViewTextBoxColumn
+            // 
+            this.selectedCourseDataGridViewTextBoxColumn.DataPropertyName = "SelectedCourse";
+            this.selectedCourseDataGridViewTextBoxColumn.HeaderText = "SelectedCourse";
+            this.selectedCourseDataGridViewTextBoxColumn.Name = "selectedCourseDataGridViewTextBoxColumn";
+            // 
+            // pictureDataGridViewImageColumn
+            // 
+            this.pictureDataGridViewImageColumn.DataPropertyName = "picture";
+            this.pictureDataGridViewImageColumn.HeaderText = "picture";
+            this.pictureDataGridViewImageColumn.Name = "pictureDataGridViewImageColumn";
+            // 
+            // CourseStudentList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 508);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnprint);
+            this.Controls.Add(this.txtnamecourse);
+            this.Controls.Add(this.semesterlabel);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "CourseStudentList";
+            this.Text = "CourseStudentList";
+            this.Load += new System.EventHandler(this.CourseStudentList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.stdBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVDBDataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVDBDataSet5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLSVDBDataSet5BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stdBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label semesterlabel;
+        private System.Windows.Forms.TextBox txtnamecourse;
+        private System.Windows.Forms.Button btnprint;
+        private QLSVDBDataSet3 qLSVDBDataSet3;
+        private System.Windows.Forms.BindingSource stdBindingSource;
+        private QLSVDBDataSet3TableAdapters.stdTableAdapter stdTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource qLSVDBDataSet5BindingSource;
+        private QLSVDBDataSet5 qLSVDBDataSet5;
+        private System.Windows.Forms.BindingSource stdBindingSource1;
+        private QLSVDBDataSet5TableAdapters.stdTableAdapter stdTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bdateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn selectedCourseDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn pictureDataGridViewImageColumn;
+    }
+}
---
+++ ListAndEditForm1/CourseNScore/CourseStudentList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListAndEditForm1.CourseNScore
+{
+    public partial class CourseStudentList : Form
+    {
+        public CourseStudentList()
+        {
+            InitializeComponent();
+        }
+
+        Student student = new Student();
+
+        private void CourseStudentList_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qLSVDBDataSet5.std' table. You can move, or remove it, as needed.
+            this.stdTableAdapter1.Fill(this.qLSVDBDataSet5.std);
+            SqlCommand command = new SqlCommand("SELECT * FROM std");
+            dataGridView1.ReadOnly = true;
+            DataGridViewImageColumn picCol = new DataGridViewImageColumn();
+            dataGridView1.RowTemplate.Height = 80;
+            dataGridView1.DataSource = student.getStudents(command);
+            picCol = (DataGridViewImageColumn)dataGridView1.Columns[9];
+            picCol.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dataGridView1.AllowUserToAddRows = false;
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnprint_Click(object sender, EventArgs e)
+        {
+            
+            SqlCommand command = new SqlCommand("SELECT * FROM std WHERE CONCAT(id,fname,lname,address,SelectedCourse)LIKE '%" + txtnamecourse.Text + "%'");
+            
+            fillGrid(command);
+        }
+
+        private void fillGrid(SqlCommand command)
+        {
+
+            dataGridView1.ReadOnly = true;
+            DataGridViewImageColumn picCol = new DataGridViewImageColumn();
+            dataGridView1.RowTemplate.Height = 80;
+            dataGridView1.DataSource = student.getStudents(command);
+            picCol = (DataGridViewImageColumn)dataGridView1.Columns[7];
+            picCol.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dataGridView1.AllowUserToAddRows = false;
+            // Dem sinh vien 
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+    }
+}
